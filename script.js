0aa352fcb86e966dc883f3a2d8a966a5355ef414
Michelle Wang <tianw0420@gmail.com>
2022-01-15 20:41:53 -0800

    added modal UI

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const display = document.querySelector('.book-display'); 
 const newBookButton = document.querySelector('.new-book-button'); 
+const modal = document.querySelector('.modal'); 
+const modalExit = document.querySelector('.exit'); 
 
 let myLibrary = []; 
 let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet'); 
@@ -29,5 +31,14 @@ function displayBooks(){
 }
 
 newBookButton.addEventListener('click', () => {
-
+    modal.style.display = 'block'; 
 })
+modalExit.addEventListener('click', () => {
+    modal.style.display = 'none'; 
+})
+window.onclick = function(event){
+    if (event.target == modal){
+        modal.style.display = 'none'; 
+    }
+}
+
