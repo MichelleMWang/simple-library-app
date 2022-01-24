b864ba7d0efe28be15470a39704e85a4e2b2d51e
Michelle Wang <tianw0420@gmail.com>
2022-01-23 21:24:29 -0800

    switched format to classes

--- script.js
+++ script.js
@@ -5,71 +5,72 @@ const modalExit = document.querySelector('.exit');
 
 const submitNewBook = document.getElementById('submit-new-book'); 
 
-let myLibrary = []; 
-
-//default books/tester books 
-let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false); 
-let theRedPyramid = new Book('The Red Pyramid', 'Rick Riordan', 357, true); 
-addBookToLibrary(theHobbit); 
-addBookToLibrary(theRedPyramid); 
-displayAllBooks();  
+let myLibrary = [];  
 
 //constructor to make new Book objects
-function Book(title, author, pages, didRead){
-    this.title = title; 
-    this.author = author; 
-    this.pages = pages; 
-    if (didRead) this.read = 'read'; 
-    else this.read = 'not read yet'; 
+class Book {
+    constructor(title, author, pages, didRead){
+        this.title = title; 
+        this.author = author; 
+        this.pages = pages; 
+        if (didRead) this.read = 'read'; 
+        else this.read = 'not read yet'; 
 
-    this.info = title + ' by ' + author + ', ' + pages + ' pages';
-    this.changeRead = () => {
-        if (this.read == 'read'){
-            this.read = 'not read yet';
-        } else this.read = 'read'; 
-    }; 
-}
+        this.info = title + ' by ' + author + ', ' + pages + ' pages';
+        this.changeRead = () => {
+            if (this.read == 'read'){
+                this.read = 'not read yet';
+            } else this.read = 'read'; 
+        }; 
+    }
+    removeBook(){
+        let index = this.bookNum; 
+        myLibrary.splice(index, 1); //removes book from array 
+        for (let i = index; i < myLibrary.length; i++){ //shifts index of items after it down 1 to fill gap 
+            myLibrary[i].index--; 
+        }
+       let bookDiv = document.querySelector(`[data-index="${index}"]`); 
+       bookDiv.remove(); 
+    }
+    //adds Books to array myLibrary
+    addBookToLibrary(){
+        myLibrary.push(this); 
+        this.bookNum = myLibrary.length - 1; 
+    }
+    //adds Book to display, displays book
+    displayBook(){
+        let bookDiv = document.createElement('div'); 
+        bookDiv.classList.add('book-div'); 
+        bookDiv.setAttribute('data-index', this.bookNum); //sets index to HTML element to be able to remove the element
+        bookDiv.textContent = this.info; 
 
-//adds Books to array myLibrary
-function addBookToLibrary(myBook){
-    myLibrary.push(myBook); 
-    myBook.bookNum = myLibrary.length - 1; 
-}
+        let changeReadButton = document.createElement('button');  
+        changeReadButton.addEventListener('click', () => {
+            this.changeRead(); 
+            changeReadButton.textContent = this.read; 
+            changeReadButton.setAttribute('read-status', this.read); //to style in CSS
+        }); 
+        changeReadButton.setAttribute('read-status', this.read);
+        changeReadButton.classList.add('change-read-button'); 
+        changeReadButton.textContent = this.read; 
+        bookDiv.appendChild(changeReadButton); 
 
-//adds Book to display, displays book
-function displayBook(myBook){
-    let bookDiv = document.createElement('div'); 
-    bookDiv.classList.add('book-div'); 
-    bookDiv.setAttribute('data-index', myBook.bookNum); //sets index to HTML element to be able to remove the element
-    bookDiv.textContent = myBook.info; 
+        let removeButton = document.createElement('button');
+        removeButton.classList.add('remove-book-button'); 
+        removeButton.addEventListener('click', () => removeBook(this)); 
+        removeButton.textContent = '✖'; 
+        bookDiv.appendChild(removeButton); 
 
-    let changeReadButton = document.createElement('button');  
-    changeReadButton.addEventListener('click', () => {
-        myBook.changeRead(); 
-        changeReadButton.textContent = myBook.read; 
-        changeReadButton.setAttribute('read-status', myBook.read); //to style in CSS
-    }); 
-    changeReadButton.setAttribute('read-status', myBook.read);
-    changeReadButton.classList.add('change-read-button'); 
-    changeReadButton.textContent = myBook.read; 
-    bookDiv.appendChild(changeReadButton); 
-
-    let removeButton = document.createElement('button');
-    removeButton.classList.add('remove-book-button'); 
-    removeButton.addEventListener('click', () => removeBook(myBook)); 
-    removeButton.textContent = '✖'; 
-    bookDiv.appendChild(removeButton); 
-
-    display.appendChild(bookDiv); 
+        display.appendChild(bookDiv); 
+    }
 }
 
 //displays all books in myLibrary 
 function displayAllBooks(){
     myLibrary.forEach(book => {
-        displayBook(book); 
+        book.displayBook(); 
     })
 }
-
 //event listeners for opening/closing modal 
 newBookButton.addEventListener('click', () => {
     modal.style.display = 'block'; 
@@ -97,19 +98,17 @@ submitNewBook.addEventListener('click', () => {
     let readInput = document.getElementById('read-input').checked;
     let newBook = new Book(titleInput, authorInput, pagesInput, readInput); 
 
-    displayBook(newBook); 
+    newBook.displayBook(); 
     modal.style.display = 'none';
     reset(); 
 })
 
+//default books/tester books 
+let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false); 
+let theRedPyramid = new Book('The Red Pyramid', 'Rick Riordan', 357, true); 
+theHobbit.addBookToLibrary(); 
+theRedPyramid.addBookToLibrary(); 
+displayAllBooks(); 
 
-function removeBook(myBook){
-    let index = myBook.bookNum; 
-    myLibrary.splice(index, 1); //removes book from array 
-    for (let i = index; i < myLibrary.length; i++){ //shifts index of items after it down 1 to fill gap 
-        myLibrary[i].index--; 
-    }
-   let bookDiv = document.querySelector(`[data-index="${index}"]`); 
-   bookDiv.remove(); 
-}
+
 
